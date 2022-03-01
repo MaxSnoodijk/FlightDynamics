1f964b4359a171efa2baf659a9c2ff848e8e27d4
Max Snoodijk <max_snoodijk@live.nl>
2022-03-01 11:52:18 +0100

    Eigenvalues for symmetric EOM.

--- solver.py
+++ solver.py
@@ -7,28 +7,28 @@ from coefficients import Coefficients
 class Solver:
 
     def __init__(self):
-        symmetric_matrix, asymmetric_matrix, misc_matrix = Coefficients()
+        symmetric_coefficients, asymmetric_coefficients, misc_coefficients = Coefficients()
 
         # 1. symmetric attributes.
-        self.C_X_0, self.C_Z_0, self.C_M_0 = symmetric_matrix[0, :]
-        self.C_X_u, self.C_Z_u, self.C_M_u = symmetric_matrix[1, :]
-        self.C_X_alpha, self.C_Z_alpha, self.C_M_alpha = symmetric_matrix[2, :]
-        self.C_X_alpha_dot, self.C_Z_alpha_dot, self.C_M_alpha_dot = symmetric_matrix[3, :]
-        self.C_X_q, self.C_Z_q, self.C_M_q = symmetric_matrix[4, :]
-        self.C_X_elevator, self.C_Z_elevator, self.C_M_elevator = symmetric_matrix[5, :]
+        self.C_X_0, self.C_Z_0, self.C_M_0 = symmetric_coefficients[0, :]
+        self.C_X_u, self.C_Z_u, self.C_M_u = symmetric_coefficients[1, :]
+        self.C_X_alpha, self.C_Z_alpha, self.C_M_alpha = symmetric_coefficients[2, :]
+        self.C_X_alpha_dot, self.C_Z_alpha_dot, self.C_M_alpha_dot = symmetric_coefficients[3, :]
+        self.C_X_q, self.C_Z_q, self.C_M_q = symmetric_coefficients[4, :]
+        self.C_X_elevator, self.C_Z_elevator, self.C_M_elevator = symmetric_coefficients[5, :]
 
         # 2. asymmetric attributes.
-        self.C_Y_beta, self.C_l_beta, self.C_n_beta = asymmetric_matrix[0, :]
-        self.C_Y_p, self.C_l_p, self.C_n_p = asymmetric_matrix[1, :]
-        self.C_Y_r, self.C_l_r, self.C_n_r = asymmetric_matrix[2, :]
-        self.C_Y_ailerons, self.C_l_ailerons, self.C_n_ailerons = asymmetric_matrix[3, :]
-        self.C_Y_rudder, self.C_l_rudder, self.C_n_rudder = asymmetric_matrix[4, :]
+        self.C_Y_beta, self.C_l_beta, self.C_n_beta = asymmetric_coefficients[0, :]
+        self.C_Y_p, self.C_l_p, self.C_n_p = asymmetric_coefficients[1, :]
+        self.C_Y_r, self.C_l_r, self.C_n_r = asymmetric_coefficients[2, :]
+        self.C_Y_ailerons, self.C_l_ailerons, self.C_n_ailerons = asymmetric_coefficients[3, :]
+        self.C_Y_rudder, self.C_l_rudder, self.C_n_rudder = asymmetric_coefficients[4, :]
 
         # 3. aerodynamic attributes.
-        self.velocity, self.density = misc_matrix[:2]
+        self.velocity, self.density = misc_coefficients[:2]
 
         # 4. aircraft attributes.
-        self.K_XX, self.K_YY, self.K_ZZ, self.K_XZ, self.m = misc_matrix[2:]
+        self.K_XX, self.K_YY, self.K_ZZ, self.K_XZ, self.m = misc_coefficients[2:]
 
         # 5. eigenvalues to be calculated.
         self.eigenvalues_symmetric = None
@@ -39,7 +39,7 @@ class Solver:
 
         u_row = [self.C_X_u - (2 * self.m * eigen),
                  self.C_X_alpha,
-                 self.C_X_0,
+                 self.C_Z_0,
                  0]
         alpha_row = [self.C_Z_u,
                      self.C_Z_alpha + ((self.C_Z_alpha_dot - (2 * self.m)) * eigen),
@@ -62,18 +62,3 @@ class Solver:
 
     def solve_asymmetric_eom(self):
         pass
-
-
-k = sym.Symbol('k')
-
-u = np.array([2, -1, k])
-v = np.array([1, 0, 2])
-w = np.array([k, 3, k])
-
-z = [u, v, w] #axis with variables
-
-#A = Matrix([u , v, w])
-A = sym.Matrix(z)
-
-equation = A.det()
-print(sym.solve(equation, k)[0])
