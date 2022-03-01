e01e6c0d259d824911afa4e0db80e879dabfe91d
Max Snoodijk <max_snoodijk@live.nl>
2022-03-01 11:47:27 +0100

    Eigenvalues for symmetric EOM.

--- solver.py
+++ solver.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sympy as sym
 
 from coefficients import Coefficients
 
@@ -27,10 +28,52 @@ class Solver:
         self.velocity, self.density = misc_matrix[:2]
 
         # 4. aircraft attributes.
-        self.K_XX, self.K_YY, self.K_ZZ, self.K_XZ = misc_matrix[2:]
+        self.K_XX, self.K_YY, self.K_ZZ, self.K_XZ, self.m = misc_matrix[2:]
+
+        # 5. eigenvalues to be calculated.
+        self.eigenvalues_symmetric = None
+        self.eigenvalues_asymmetric = None
 
     def solve_symmetric_eom(self):
-        pass
+        eigen = sym.Symbol['eigen']
+
+        u_row = [self.C_X_u - (2 * self.m * eigen),
+                 self.C_X_alpha,
+                 self.C_X_0,
+                 0]
+        alpha_row = [self.C_Z_u,
+                     self.C_Z_alpha + ((self.C_Z_alpha_dot - (2 * self.m)) * eigen),
+                     - self.C_X_0,
+                     self.C_Z_q + (2 * self.m)]
+        theta_row = [0,
+                     0,
+                     -eigen,
+                     1]
+        q_row = [self.C_M_u,
+                 self.C_M_alpha + (self.C_M_alpha_dot * eigen),
+                 0,
+                 self.C_M_q - (2 * self.m * self.K_YY * eigen)]
+
+        symmetric_eom_np = np.array([u_row, alpha_row, theta_row, q_row])
+        symmetric_eom_sym = sym.Matrix(symmetric_eom_np)
+        symmetric_det = symmetric_eom_sym.det()
+
+        self.eigenvalues_symmetric = sym.solve(symmetric_det)
 
     def solve_asymmetric_eom(self):
         pass
+
+
+k = sym.Symbol('k')
+
+u = np.array([2, -1, k])
+v = np.array([1, 0, 2])
+w = np.array([k, 3, k])
+
+z = [u, v, w] #axis with variables
+
+#A = Matrix([u , v, w])
+A = sym.Matrix(z)
+
+equation = A.det()
+print(sym.solve(equation, k)[0])
