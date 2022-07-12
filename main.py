da003f834872cc78275aafa4e8307f5e22ea0a30
Max Snoodijk <max_snoodijk@live.nl>
2022-07-12 15:50:01 +0200

    Final code commit.

---
+++ main.py
@@ -0,0 +1,10 @@
+from Code.solver import main
+
+# Folder in which the aircraft is located.
+path = 'Aircraft/Asymmetric'
+
+# Analysis with fuselage effects in case True.
+fuselage = False
+
+# Run the entire program.
+main(path, fuselage=fuselage)
